f517fda6978cf79201d20abab6941c4807296fb9
harman bhullar <harman76.bhullar@gmail.com>
2019-05-04 15:08:47 -0500

    commit

--- 01-webotron/webtron/webtron.py
+++ 01-webotron/webtron/webtron.py
@@ -1,6 +1,9 @@
 import boto3
 import click
 from botocore.exceptions import ClientError
+from pathlib import Path
+import mimetypes
+
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
 
@@ -62,6 +65,32 @@ def setup_bucket(bucket):
     })
 
     return
+
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+            'ContentType': 'text/html'
+        })
+
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Sync contentof PATHNAME to BUCKET"
+    s3_bucket = s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir(): handle_directory(p)
+#            if p.is_file(): print("Path: {}\n Key:{}".format(p, p.relative_to(root)))
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+
+    handle_directory(root)
 
 if __name__ == '__main__':
     cli()
